ac355a4af1738b6da720c8d46d48e7271568455a
Andrii Krenevych <krenevych@knu.ua>
2025-03-31 20:44:18 +0300

    shaders

--- src/main.cpp
+++ src/main.cpp
@@ -33,13 +33,51 @@ int main(void)
 
     glClearColor(1.0, 0.0, 0.0, 1.0);
 
-    /* Loop until the user closes the window */
+    //  підготовка шейдерів
+    auto vertexShaderCode = R"(
+        #version 330 core
+        layout(location = 0) in vec3 aPos;
+        void main() {
+            gl_Position = vec4(aPos, 1.0);
+        }
+    )";
+    auto fragmentShaderCode = R"(
+        #version 330 core
+        out vec4 FragColor;
+        void main() {
+            FragColor = vec4(0.0, 1.0, 0.0, 1.0);  // зелений
+        }
+    )";
 
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderCode, nullptr);
+    glCompileShader(vertexShader);
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderCode, nullptr);
+    glCompileShader(fragmentShader);
+
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    float vertices[] = {
+        -0.5f, -0.5f, 0.f,
+         0.5f, -0.5f, 0.f,
+         0.0f,  0.5f, 0.f,
+    };
+
+    /* Loop until the user closes the window */
     do {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
         // Rendering - gl*-function calls
+        glUseProgram(shaderProgram);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
