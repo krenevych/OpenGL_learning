f1017d815f6e4d07f89729c7a23e6052aa6bca5a
Andrii Krenevych <krenevych@knu.ua>
2025-03-31 11:53:42 +0300

    додано параметри для створення gl-контексту

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,11 @@ int main(void)
     /* Initialize the library */
     if (!glfwInit())
         return -1;
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // для macOS
+
 
     /* Create a windowed mode window and its OpenGL context */
     GLFWwindow *window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
@@ -21,8 +26,7 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
@@ -30,17 +34,22 @@ int main(void)
     glClearColor(1.0, 0.0, 0.0, 1.0);
 
     /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
+
+    do {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        // Rendering - gl*-function calls
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
         /* Poll for and process events */
         glfwPollEvents();
-    }
+    } while (!glfwWindowShouldClose(window) &&
+        glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS);
+
+    // очистка ресурсів з відеокарти
 
     glfwTerminate();
     return 0;
