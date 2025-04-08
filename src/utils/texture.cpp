0b0ee965a4df0c8214cfffff77f0610b24c1a69f
Andrii Krenevych <krenevych@knu.ua>
2025-04-08 11:57:52 +0300

    texture loading

---
+++ src/utils/texture.cpp
@@ -0,0 +1,39 @@
+//
+// Created by Andrii Krenevych on 08.04.2025.
+//
+#include <string>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <iostream>
+
+#include "stb_image.h"
+#include "glad/glad.h"
+
+unsigned int loadTexture(const std::string& fileName){
+   // засилання даних на відеокарту та отримання id текстури
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    // set the texture wrapping/filtering options (on the currently bound texture object)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // load and generate the texture
+    int width, height, nrChannels;
+    unsigned char *data = stbi_load(fileName.c_str(), &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+
+
+  stbi_image_free(data);
+  return 0;
+}
---
+++ src/utils/texture.h
@@ -0,0 +1,14 @@
+//
+// Created by Andrii Krenevych on 08.04.2025.
+//
+
+#pragma once
+
+// #ifndef TEXTURE_H
+// #define TEXTURE_H
+
+#include  <string>
+
+unsigned int loadTexture(const std::string& fileName);
+
+// #endif //TEXTURE_H
